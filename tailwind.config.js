cf8ffb9e4cda099e3e2af3af60410576d33445e8
Matt Reed <matthewreed26@gmail.com>
2023-02-25 18:14:29 -0500

    missing functionality from flowbite install

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/main/webapp/index.html', './src/main/webapp/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
+};
